4658d99bcfcdde9bc7a049947fa3c2e6eda60bad
Maxim Ryabov <maxim.ryabov@rokolabs.com>
2025-08-12 15:57:15 +0200

    fix multiple autocomplete

--- _src/shared/ui/components/chip/component.tsx
+++ _src/shared/ui/components/chip/component.tsx
@@ -20,7 +20,7 @@ const chip = cva(
 		"h-[21px] min-w-fit pl-2 pr-1",
 		"flex flex-row items-center",
 		"bg-neutral-200",
-		"rounded-sm text-xs transition-all select-none outline-primary-500",
+		"rounded-sm text-xs transition-colors select-none outline-primary-500",
 	],
 	{
 		variants: {
@@ -31,6 +31,9 @@ const chip = cva(
 			disabled: {
 				true: ["text-neutral-600 hover:cursor-not-allowed"],
 				false: ["cursor-pointer text-neutral-900"],
+			},
+			isFocused: {
+				true: ["outline-2"],
 			},
 		},
 		compoundVariants: [
@@ -55,8 +58,12 @@ export function Chip(props: ChipProps) {
 	return (
 		<Tag
 			{...restProps}
-			className={({ isDisabled }) =>
-				chip({ outlined, disabled: restProps.isDisabled || isDisabled })
+			className={({ isDisabled, isFocusVisible, isFocused }) =>
+				chip({
+					outlined,
+					disabled: restProps.isDisabled || isDisabled,
+					isFocused: isFocusVisible || isFocused,
+				})
 			}
 			aria-label={`${props.textValue} chip`}
 		>
